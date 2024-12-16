f540a96f0ea0a6105f1c89eca1c84278618b417b
Owercone <131294751+owercone@users.noreply.github.com>
2024-12-16 20:42:25 +0300

    Add files via upload

---
+++ Project-master/MainProject/MainForm.h
@@ -0,0 +1,239 @@
+﻿#pragma once
+
+namespace MainProject {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// Сводка для MainForm
+	/// </summary>
+	public ref class MainForm : public System::Windows::Forms::Form
+	{
+	public:
+		MainForm(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: добавьте код конструктора
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// Освободить все используемые ресурсы.
+		/// </summary>
+		~MainForm()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+
+
+	private: System::Windows::Forms::Label^ label1;
+
+
+	private: System::Windows::Forms::CheckedListBox^ checkedListBox1;
+	private: System::Windows::Forms::Button^ button1;
+	private: System::Windows::Forms::Panel^ panel1;
+	private: bool open = false;
+	private: System::Windows::Forms::Button^ button3;
+	private: System::Windows::Forms::Label^ label3;
+	protected:
+
+	protected:
+
+	private:
+		/// <summary>
+		/// Обязательная переменная конструктора.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Требуемый метод для поддержки конструктора — не изменяйте 
+		/// содержимое этого метода с помощью редактора кода.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->checkedListBox1 = (gcnew System::Windows::Forms::CheckedListBox());
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->panel1 = (gcnew System::Windows::Forms::Panel());
+			this->label3 = (gcnew System::Windows::Forms::Label());
+			this->button3 = (gcnew System::Windows::Forms::Button());
+			this->panel1->SuspendLayout();
+			this->SuspendLayout();
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 20.75F));
+			this->label1->ForeColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(246)), static_cast<System::Int32>(static_cast<System::Byte>(246)),
+				static_cast<System::Int32>(static_cast<System::Byte>(246)));
+			this->label1->Location = System::Drawing::Point(30, 12);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(421, 64);
+			this->label1->TabIndex = 2;
+			this->label1->Text = L"Удаление ненужных программ\r\nОтключение телеметрии ";
+			this->label1->Click += gcnew System::EventHandler(this, &MainForm::label1_Click);
+			// 
+			// checkedListBox1
+			// 
+			this->checkedListBox1->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(55)), static_cast<System::Int32>(static_cast<System::Byte>(55)),
+				static_cast<System::Int32>(static_cast<System::Byte>(55)));
+			this->checkedListBox1->BorderStyle = System::Windows::Forms::BorderStyle::FixedSingle;
+			this->checkedListBox1->ForeColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(246)), static_cast<System::Int32>(static_cast<System::Byte>(246)),
+				static_cast<System::Int32>(static_cast<System::Byte>(246)));
+			this->checkedListBox1->FormattingEnabled = true;
+			this->checkedListBox1->Items->AddRange(gcnew cli::array< System::Object^  >(23) {
+				L"Yandex.Music", L"ZuneMusic", L"XboxApp",
+					L"XboxGameOverlay", L"XboxGamingOverlay", L"Xbox.TCUI", L"XboxSpeechToTextOverlay", L"XboxGameCallableUI", L"People", L"SkypeApp",
+					L"GetHelp", L"StickyNotes", L"MixedReality.Portal", L"BingWeather", L"WindowsMaps", L"MSPaint", L"MicrosoftOneNote", L"Cortana",
+					L"ParentalControl", L"PeopleExperienceHost", L"OneNote", L"FeedBackHub", L"SolitaireCollection"
+			});
+			this->checkedListBox1->Location = System::Drawing::Point(36, 120);
+			this->checkedListBox1->Margin = System::Windows::Forms::Padding(0);
+			this->checkedListBox1->Name = L"checkedListBox1";
+			this->checkedListBox1->Size = System::Drawing::Size(382, 347);
+			this->checkedListBox1->TabIndex = 5;
+			this->checkedListBox1->Visible = false;
+			this->checkedListBox1->SelectedIndexChanged += gcnew System::EventHandler(this, &MainForm::checkedListBox1_SelectedIndexChanged);
+			// 
+			// button1
+			// 
+			this->button1->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(246)), static_cast<System::Int32>(static_cast<System::Byte>(246)),
+				static_cast<System::Int32>(static_cast<System::Byte>(246)));
+			this->button1->FlatAppearance->BorderSize = 0;
+			this->button1->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
+			this->button1->ForeColor = System::Drawing::Color::Black;
+			this->button1->Location = System::Drawing::Point(370, 88);
+			this->button1->Margin = System::Windows::Forms::Padding(0);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(48, 32);
+			this->button1->TabIndex = 6;
+			this->button1->Text = L"V";
+			this->button1->UseVisualStyleBackColor = false;
+			this->button1->Click += gcnew System::EventHandler(this, &MainForm::button1_Click);
+			// 
+			// panel1
+			// 
+			this->panel1->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(55)), static_cast<System::Int32>(static_cast<System::Byte>(55)),
+				static_cast<System::Int32>(static_cast<System::Byte>(55)));
+			this->panel1->BorderStyle = System::Windows::Forms::BorderStyle::FixedSingle;
+			this->panel1->Controls->Add(this->label3);
+			this->panel1->Location = System::Drawing::Point(36, 88);
+			this->panel1->Margin = System::Windows::Forms::Padding(0);
+			this->panel1->Name = L"panel1";
+			this->panel1->Size = System::Drawing::Size(334, 32);
+			this->panel1->TabIndex = 7;
+			this->panel1->Paint += gcnew System::Windows::Forms::PaintEventHandler(this, &MainForm::panel1_Paint);
+			// 
+			// label3
+			// 
+			this->label3->AutoSize = true;
+			this->label3->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 9.75F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->label3->Location = System::Drawing::Point(3, 2);
+			this->label3->Name = L"label3";
+			this->label3->Size = System::Drawing::Size(57, 16);
+			this->label3->TabIndex = 0;
+			this->label3->Text = L"Список:";
+			// 
+			// button3
+			// 
+			this->button3->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(55)), static_cast<System::Int32>(static_cast<System::Byte>(55)),
+				static_cast<System::Int32>(static_cast<System::Byte>(55)));
+			this->button3->FlatAppearance->BorderSize = 0;
+			this->button3->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
+			this->button3->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 9.75F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->button3->ForeColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(246)), static_cast<System::Int32>(static_cast<System::Byte>(246)),
+				static_cast<System::Int32>(static_cast<System::Byte>(246)));
+			this->button3->Location = System::Drawing::Point(36, 482);
+			this->button3->Margin = System::Windows::Forms::Padding(0);
+			this->button3->Name = L"button3";
+			this->button3->Size = System::Drawing::Size(377, 72);
+			this->button3->TabIndex = 8;
+			this->button3->Text = L"Выполнить";
+			this->button3->UseVisualStyleBackColor = false;
+			this->button3->Click += gcnew System::EventHandler(this, &MainForm::button3_Click);
+			// 
+			// MainForm
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(33)), static_cast<System::Int32>(static_cast<System::Byte>(33)),
+				static_cast<System::Int32>(static_cast<System::Byte>(33)));
+			this->ClientSize = System::Drawing::Size(474, 580);
+			this->Controls->Add(this->button3);
+			this->Controls->Add(this->label1);
+			this->Controls->Add(this->checkedListBox1);
+			this->Controls->Add(this->button1);
+			this->Controls->Add(this->panel1);
+			this->Cursor = System::Windows::Forms::Cursors::Default;
+			this->ForeColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(246)), static_cast<System::Int32>(static_cast<System::Byte>(246)),
+				static_cast<System::Int32>(static_cast<System::Byte>(246)));
+			this->FormBorderStyle = System::Windows::Forms::FormBorderStyle::FixedSingle;
+			this->MaximumSize = System::Drawing::Size(1400, 1050);
+			this->MinimumSize = System::Drawing::Size(300, 321);
+			this->Name = L"MainForm";
+			this->Text = L"Programm";
+			this->panel1->ResumeLayout(false);
+			this->panel1->PerformLayout();
+			this->ResumeLayout(false);
+			this->PerformLayout();
+
+		}
+#pragma endregion
+
+		private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) 
+		{
+
+			if (!open) 
+			{
+				this->checkedListBox1->Visible = true;
+				open = true;
+			}
+
+			else
+			{
+				this->checkedListBox1->Visible = false;
+				open = false;
+			}
+		}
+
+		private: System::Void checkedListBox1_SelectedIndexChanged(System::Object^ sender, System::EventArgs^ e) 
+		{
+		
+		}
+
+		private: System::Void button3_Click(System::Object^ sender, System::EventArgs^ e) 
+		{
+			//if (this->checkedListBox1->GetItemChecked(0)) элемент выпадащего списка
+			//{
+			//	//Delete
+			//}
+			//if (this->checkedListBox1->GetItemChecked(1))
+			//{
+			//	//Delete
+			//}
+			//if (this->checkedListBox1->GetItemChecked(2))
+			//{
+			//	//Delete
+			//} и тд
+		}
+private: System::Void panel1_Paint(System::Object^ sender, System::Windows::Forms::PaintEventArgs^ e) {
+}
+private: System::Void label2_Click(System::Object^ sender, System::EventArgs^ e) {
+}
+private: System::Void label1_Click(System::Object^ sender, System::EventArgs^ e) {
+}
+};
+} 
