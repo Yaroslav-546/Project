d1d1dc7686b7c0d1a74642da21b14102b61d4d6f
Owercone <131294751+owercone@users.noreply.github.com>
2024-12-19 18:06:54 +0300

    Add files via upload

--- MainProject/MainForm.h
+++ MainProject/MainForm.h
@@ -37,7 +37,7 @@ namespace MainProject {
 
 
 	private: System::Windows::Forms::Label^ label1;
-
+	private: System::Windows::Forms::Label^ label2;
 
 	private: System::Windows::Forms::CheckedListBox^ checkedListBox1;
 	private: System::Windows::Forms::Button^ button1;
@@ -63,6 +63,7 @@ namespace MainProject {
 		void InitializeComponent(void)
 		{
 			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->label2 = (gcnew System::Windows::Forms::Label());
 			this->checkedListBox1 = (gcnew System::Windows::Forms::CheckedListBox());
 			this->button1 = (gcnew System::Windows::Forms::Button());
 			this->panel1 = (gcnew System::Windows::Forms::Panel());
@@ -74,21 +75,35 @@ namespace MainProject {
 			// label1
 			// 
 			this->label1->AutoSize = true;
-			this->label1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 20.75F));
+			this->label1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 15.75F));
 			this->label1->ForeColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(246)), static_cast<System::Int32>(static_cast<System::Byte>(246)),
 				static_cast<System::Int32>(static_cast<System::Byte>(246)));
-			this->label1->Location = System::Drawing::Point(30, 12);
+			this->label1->Location = System::Drawing::Point(80, 55);
+			this->label1->Margin = System::Windows::Forms::Padding(0);
 			this->label1->Name = L"label1";
-			this->label1->Size = System::Drawing::Size(421, 64);
+			this->label1->Size = System::Drawing::Size(319, 25);
 			this->label1->TabIndex = 2;
-			this->label1->Text = L"Удаление ненужных программ\r\nОтключение телеметрии ";
-			this->label1->Click += gcnew System::EventHandler(this, &MainForm::label1_Click);
+			this->label1->Text = L"Удаление ненужных программ";
+			// 
+			// label2
+			// 
+			this->label2->AutoSize = true;
+			this->label2->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 15.75F));
+			this->label2->ForeColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(246)), static_cast<System::Int32>(static_cast<System::Byte>(246)),
+				static_cast<System::Int32>(static_cast<System::Byte>(246)));
+			this->label2->Location = System::Drawing::Point(109, 87);
+			this->label2->Margin = System::Windows::Forms::Padding(0);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(272, 25);
+			this->label2->TabIndex = 3;
+			this->label2->Text = L"И Оключение телеметрии";
 			// 
 			// checkedListBox1
 			// 
 			this->checkedListBox1->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(55)), static_cast<System::Int32>(static_cast<System::Byte>(55)),
 				static_cast<System::Int32>(static_cast<System::Byte>(55)));
-			this->checkedListBox1->BorderStyle = System::Windows::Forms::BorderStyle::FixedSingle;
+			this->checkedListBox1->BorderStyle = System::Windows::Forms::BorderStyle::None;
+			this->checkedListBox1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 13.25F));
 			this->checkedListBox1->ForeColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(246)), static_cast<System::Int32>(static_cast<System::Byte>(246)),
 				static_cast<System::Int32>(static_cast<System::Byte>(246)));
 			this->checkedListBox1->FormattingEnabled = true;
@@ -98,13 +113,12 @@ namespace MainProject {
 					L"GetHelp", L"StickyNotes", L"MixedReality.Portal", L"BingWeather", L"WindowsMaps", L"MSPaint", L"MicrosoftOneNote", L"Cortana",
 					L"ParentalControl", L"PeopleExperienceHost", L"OneNote", L"FeedBackHub", L"SolitaireCollection"
 			});
-			this->checkedListBox1->Location = System::Drawing::Point(36, 120);
+			this->checkedListBox1->Location = System::Drawing::Point(73, 219);
 			this->checkedListBox1->Margin = System::Windows::Forms::Padding(0);
 			this->checkedListBox1->Name = L"checkedListBox1";
-			this->checkedListBox1->Size = System::Drawing::Size(382, 347);
+			this->checkedListBox1->Size = System::Drawing::Size(342, 242);
 			this->checkedListBox1->TabIndex = 5;
 			this->checkedListBox1->Visible = false;
-			this->checkedListBox1->SelectedIndexChanged += gcnew System::EventHandler(this, &MainForm::checkedListBox1_SelectedIndexChanged);
 			// 
 			// button1
 			// 
@@ -113,10 +127,10 @@ namespace MainProject {
 			this->button1->FlatAppearance->BorderSize = 0;
 			this->button1->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
 			this->button1->ForeColor = System::Drawing::Color::Black;
-			this->button1->Location = System::Drawing::Point(370, 88);
+			this->button1->Location = System::Drawing::Point(364, 167);
 			this->button1->Margin = System::Windows::Forms::Padding(0);
 			this->button1->Name = L"button1";
-			this->button1->Size = System::Drawing::Size(48, 32);
+			this->button1->Size = System::Drawing::Size(51, 51);
 			this->button1->TabIndex = 6;
 			this->button1->Text = L"V";
 			this->button1->UseVisualStyleBackColor = false;
@@ -128,10 +142,11 @@ namespace MainProject {
 				static_cast<System::Int32>(static_cast<System::Byte>(55)));
 			this->panel1->BorderStyle = System::Windows::Forms::BorderStyle::FixedSingle;
 			this->panel1->Controls->Add(this->label3);
-			this->panel1->Location = System::Drawing::Point(36, 88);
+			this->panel1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 12.225F));
+			this->panel1->Location = System::Drawing::Point(73, 167);
 			this->panel1->Margin = System::Windows::Forms::Padding(0);
 			this->panel1->Name = L"panel1";
-			this->panel1->Size = System::Drawing::Size(334, 32);
+			this->panel1->Size = System::Drawing::Size(308, 52);
 			this->panel1->TabIndex = 7;
 			this->panel1->Paint += gcnew System::Windows::Forms::PaintEventHandler(this, &MainForm::panel1_Paint);
 			// 
@@ -152,14 +167,13 @@ namespace MainProject {
 				static_cast<System::Int32>(static_cast<System::Byte>(55)));
 			this->button3->FlatAppearance->BorderSize = 0;
 			this->button3->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
-			this->button3->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 9.75F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
-				static_cast<System::Byte>(204)));
+			this->button3->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 13.75F));
 			this->button3->ForeColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(246)), static_cast<System::Int32>(static_cast<System::Byte>(246)),
 				static_cast<System::Int32>(static_cast<System::Byte>(246)));
-			this->button3->Location = System::Drawing::Point(36, 482);
+			this->button3->Location = System::Drawing::Point(73, 480);
 			this->button3->Margin = System::Windows::Forms::Padding(0);
 			this->button3->Name = L"button3";
-			this->button3->Size = System::Drawing::Size(377, 72);
+			this->button3->Size = System::Drawing::Size(344, 95);
 			this->button3->TabIndex = 8;
 			this->button3->Text = L"Выполнить";
 			this->button3->UseVisualStyleBackColor = false;
@@ -171,11 +185,12 @@ namespace MainProject {
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(33)), static_cast<System::Int32>(static_cast<System::Byte>(33)),
 				static_cast<System::Int32>(static_cast<System::Byte>(33)));
-			this->ClientSize = System::Drawing::Size(474, 580);
+			this->ClientSize = System::Drawing::Size(492, 634);
 			this->Controls->Add(this->button3);
-			this->Controls->Add(this->label1);
 			this->Controls->Add(this->checkedListBox1);
 			this->Controls->Add(this->button1);
+			this->Controls->Add(this->label2);
+			this->Controls->Add(this->label1);
 			this->Controls->Add(this->panel1);
 			this->Cursor = System::Windows::Forms::Cursors::Default;
 			this->ForeColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(246)), static_cast<System::Int32>(static_cast<System::Byte>(246)),
@@ -185,6 +200,7 @@ namespace MainProject {
 			this->MinimumSize = System::Drawing::Size(300, 321);
 			this->Name = L"MainForm";
 			this->Text = L"Programm";
+			this->Load += gcnew System::EventHandler(this, &MainForm::MainForm_Load);
 			this->panel1->ResumeLayout(false);
 			this->panel1->PerformLayout();
 			this->ResumeLayout(false);
@@ -209,11 +225,6 @@ namespace MainProject {
 			}
 		}
 
-		private: System::Void checkedListBox1_SelectedIndexChanged(System::Object^ sender, System::EventArgs^ e) 
-		{
-		
-		}
-
 		private: System::Void button3_Click(System::Object^ sender, System::EventArgs^ e) 
 		{
 			//if (this->checkedListBox1->GetItemChecked(0)) элемент выпадащего списка
@@ -228,12 +239,11 @@ namespace MainProject {
 			//{
 			//	//Delete
 			//} и тд
+			//отключение телеметрии
 		}
+private: System::Void MainForm_Load(System::Object^ sender, System::EventArgs^ e) {
+}
 private: System::Void panel1_Paint(System::Object^ sender, System::Windows::Forms::PaintEventArgs^ e) {
-}
-private: System::Void label2_Click(System::Object^ sender, System::EventArgs^ e) {
-}
-private: System::Void label1_Click(System::Object^ sender, System::EventArgs^ e) {
 }
 };
 } 
